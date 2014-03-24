5e24158bb4c338ae5a59b93ce89fc33a76862b49
Benji Marshall <benji.john.marshall@gmail.com>
2014-03-24 13:42:41 +0000

    Added basic window and progress bar

--- anticake/AntiCake.java
+++ anticake/AntiCake.java
@@ -3,15 +3,26 @@ package anticake;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
 
 public class AntiCake {
 	public static void main(String args[]) {
 		File homedir = new File("./");
 		ArrayList<String> names = new ArrayList<String>(Arrays.asList(homedir.list()));
 		
+    JFrame frame = new JFrame("AntiCake");
+    JProgressBar progressBar = new JProgressBar(0, names.size());
+    progressBar.setValue(0);
+    progressBar.setStringPainted(true);
+    frame.add(progressBar);
+    frame.pack();
+    frame.setVisible(true);
+    
 		for (int x = 0; x < names.size(); x++) {
 			if (names.get(x).startsWith("Cake")) {
 				(new File("./" + names.get(x))).delete();
+        progressBar.setValue(x);
 			}
 		}
 	}
